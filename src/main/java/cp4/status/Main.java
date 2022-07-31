cee9648101dec8d64aad8235c1b28145d37552a9
oasis4 <67016368+oasis4@users.noreply.github.com>
2022-07-31 20:42:08 +0200

    fix

--- src/main/java/cp4/status/Main.java
+++ src/main/java/cp4/status/Main.java
@@ -15,7 +15,6 @@ import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
