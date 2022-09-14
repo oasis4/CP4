e0adbac7e6112d473869c050c8daafd9c6482b41
Bastian <lebaasti02@gmail.com>
2022-09-14 17:39:39 +0200

    removed custom Prefix added Status.kt

--- src/main/kotlin/de/lebaasti/craftprotect4/functions/PlayerFunctions.kt
+++ src/main/kotlin/de/lebaasti/craftprotect4/functions/PlayerFunctions.kt
@@ -2,10 +2,22 @@ package de.lebaasti.craftprotect4.functions
 
 import org.bukkit.Statistic
 import org.bukkit.entity.Player
+import java.util.StringJoiner
 
+val playerStatus = mutableMapOf<Player, Status>()
+
+var Player.status: Status
+    get() {
+        return playerStatus[this] ?: Status.NONE
+    }
+    set(value) {
+        playerStatus[this] = value
+        this.updateTablist()
+    }
 val Player.deaths get() = getStatistic(Statistic.DEATHS)
 
 fun Player.updateTablist(deaths: Int = this.getStatistic(Statistic.DEATHS)) {
     val group = group ?: return
-    this.setPlayerListName("${group.displayName} §7${this.name} §8(§c$deaths ☠§8)")
+    this.setPlayerListName("${status.displayString} ${group.prefix}${group.displayName} §8| §7${this.name} §8(§c$deaths ☠§8)")
 }
+
