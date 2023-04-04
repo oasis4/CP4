bfa062caec21d5b4bd32086e1dadff5b4441e70a
oasis4 <67016368+oasis4@users.noreply.github.com>
2023-04-04 17:25:42 +0200

    Try to make a gui

--- src/main/java/com/github/oasis/craftprotect/command/LiveCommand.java
+++ src/main/java/com/github/oasis/craftprotect/command/LiveCommand.java
@@ -3,16 +3,25 @@ package com.github.oasis.craftprotect.command;
 import com.github.oasis.craftprotect.CraftProtectPlugin;
 import com.github.oasis.craftprotect.api.CraftProtectCommand;
 import com.github.oasis.craftprotect.utils.M;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 /**
  * Gives the player the rank "live" in the tablist
  */
 public class LiveCommand implements CraftProtectCommand {
 
+    private Inventory inv;
     private final CraftProtectPlugin plugin;
 
     public LiveCommand(CraftProtectPlugin plugin) {
@@ -28,8 +37,9 @@ public class LiveCommand implements CraftProtectCommand {
         }
         if (!(sender instanceof Player player)) {
             plugin.sendMessage(sender, M.NO_PLAYER);
-            return false;
+            return true;
         }
+
 
 
         return true;
